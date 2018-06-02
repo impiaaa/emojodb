e7855d81ad2f31b23bf25061cf5b7e6ad1bfe0b2
Spencer Alves <impiaaa@gmail.com>
2018-06-02 10:04:47 -0700

    Config from object

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask("emojodb")
-import config
+app.config.from_object('config')
 
 def refresh_all_instances():
 	from models import Instance
